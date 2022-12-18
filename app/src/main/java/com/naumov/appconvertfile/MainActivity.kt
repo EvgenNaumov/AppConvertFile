6a706a7b1bf580b4d8a844a286a79469cabfdc8e
Evgen Naumov <naumovev@mail.ru>
2022-12-19 00:12:21 +0300

    Initial commit

---
+++ app/src/main/java/com/naumov/appconvertfile/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.naumov.appconvertfile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import moxy.MvpAppCompatActivity
+
+class MainActivity : MvpAppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
